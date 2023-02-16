f994982e373bd921acc77e3e1850ff8106f87ba9
Douglas <douglas.developer.app@gmail.com>
2023-02-15 22:20:26 -0300

    função print

--- aula2.py
+++ aula2.py
@@ -1,2 +1,26 @@
-print(12,34, sep='.')
-print(44,54, sep="-")
+print(12, 34, 100, sep='.')
+print(44, 54, sep="-")
+print(44, 54, sep="-")
+
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Douglas Coelho')
+print(1, 'Douglas "Coelho"')
+
+# Aspas duplas
+print("Douglas Coelho")
+print(2, "Douglas 'Coelho'")
+
+# Escape
+print("Douglas \"Coelho\"")
+
+# r
+print(r"Douglas \"Coelho\"")
