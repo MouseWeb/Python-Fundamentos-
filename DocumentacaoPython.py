bc3987f0ad58ca89ff7d6cd3b8372520a3ffa70e
Douglas <douglas.developer.app@gmail.com>
2023-02-17 03:35:16 -0300

    Documentação do Python

---
+++ DocumentacaoPython.py
@@ -0,0 +1,12 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Tipos Imutáveis: str, int, float, bool
+
+https://pep8.org/
+"""
+string = '1000'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_variavel)
+print(string.zfill(10))
+
