75f7dd069903679081b82d9a6858f17285db1c3b
Douglas <douglas.developer.app@gmail.com>
2023-02-17 04:02:35 -0300

    while

---
+++ while.py
@@ -0,0 +1,212 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+d
+print('Acabou')
+
+
+print('############################################################')
+
+
+contador = 0
+
+while contador <= 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
+
+
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 10
+
+###
+
+contador /= 5
+print(contador)
+
+
+print('############################################################')
+
+
+contador = 0
+
+while contador <= 100:
+    contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6.')
+        continue
+
+    if contador >= 10 and contador <= 27:
+        print('Não vou mostrar o', contador)
+        continue
+
+    print(contador)
+
+    if contador == 40:
+        break
+
+
+print('Acabou')
+
+
+print('############################################################')
+
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
+
+
+print('Acabou')
+
+
+print('############################################################')
+
+
+"""
+Iterando strings com while
+"""
+#       012345678910
+# nome = 'Douglas Coelho'  # Iteráveis
+#      1110987654321
+
+
+nome = 'Maria Helena'  # Iteráveis
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
+
+
+print('############################################################')
+
+
+""" Calculadora com while """
+while True:
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-/*): ')
+
+    numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    print('Realizando sua conta. Confira o resultado abaixo:')
+
+    if operador == '+':
+        print(f'{num_1_float}+{num_2_float}=', num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'{num_1_float}-{num_2_float}=', num_1_float - num_2_float)
+    elif operador == '/':
+        print(f'{num_1_float}/{num_2_float}=', num_1_float / num_2_float)
+    elif operador == '*':
+        print(f'{num_1_float}*{num_2_float}=', num_1_float * num_2_float)
+    else:
+        print('Nunca deveria chegar aqui.')
+
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
+
+
+print('############################################################')
+
+
+""" while/else """
+string = 'Valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string.')
+    
+print('Fora do while.')
+
+
+print('############################################################')
+
+
+frase = 'aaaaooo'
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes <= qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
